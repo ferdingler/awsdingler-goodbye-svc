26a5e748176e4be16526b7d9a1ec5b610d96c97a
Fernando Dingler <fdingler@amazon.com>
2019-07-11 17:42:14 -0700

    Added main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/bye", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Good Bye: " + time.Now().Format(time.RFC1123),
+		})
+	})
+
+	router.Run(":8080")
+}
